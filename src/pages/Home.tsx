a75e642bbfce878dce2cad34d56f4573cce2aca3
artems <artem.shundryk@playtech.com>
2025-04-06 09:54:52 +0300

    hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../layout/Layout';
-import Hero from '../components/hero';
+import Hero from '../components/Hero';
 import NextMatch from '../components/NextMatch';
 
 const Home = () => (
