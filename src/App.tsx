8fe166d7072d89e44926c40fcb787d1d9c438074
artems <artem.shundryk@playtech.com>
2025-04-12 16:23:13 +0300

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import NotFound from "./components/NotFound";
 import Home from './pages/Home';
 import Fanshop from './pages/Fanshop';
@@ -7,10 +7,21 @@ import PasswordGate from './PasswordGate';
 import Matches from './pages/Matches';
 import Squad from './pages/Squad';
 
+function ScrollToTop() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <PasswordGate>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/fanshop" element={<Fanshop />} />
